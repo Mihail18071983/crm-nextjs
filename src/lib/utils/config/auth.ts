1ce9795508655d482252035f9e1e0eade169cc4e
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2024-02-05 10:51:39 +0200

    upd:add signin

---
+++ src/lib/utils/config/auth.ts
@@ -0,0 +1,96 @@
+import { NextAuthOptions } from 'next-auth';
+import User from '@/lib/utils/models/auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import bycrptjs from 'bcryptjs';
+import { connect } from '@/lib/utils/config/dbConfig';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'email', required: true },
+        password: { label: 'password', type: 'password', required: true },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        try {
+          await connect();
+          const user = await User.findOne({ email });
+          if (!user) {
+            return null;
+          }
+          const passwordsMatch = await bycrptjs.compare(
+            password,
+            user.password,
+          );
+          if (!passwordsMatch) {
+            return null;
+          }
+          return user;
+        } catch (error) {
+          console.log('Error:', error);
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+
+  session: {
+    strategy: 'jwt',
+  },
+
+  callbacks: {
+    async signIn({ user, account }: { user: any; account: any }) {
+      if (account.provider === 'google') {
+        try {
+          const { name, email } = user;
+          await connect();
+          const ifUserExists = await User.findOne({ email });
+          if (ifUserExists) {
+            return user;
+          }
+          const newUser = new User({
+            name: name,
+            email: email,
+          });
+          const res = await newUser.save();
+          if (res.status === 200 || res.status === 201) {
+            console.log(res);
+            return user;
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      return user;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.email = user.email;
+        token.name = user.name;
+      }
+      return token;
+    },
+
+    async session({ session, token }: { session: any; token: any }) {
+      if (session.user) {
+        session.user.email = token.email;
+        session.user.name = token.name;
+      }
+      console.log(session);
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+  pages: {
+    signIn: '/signin',
+  },
+};
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+// import { getToken } from "next-auth/jwt";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname;
+//   const token = await getToken({
+//     req: req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
+
+//   const publicPaths = path === "/" || path === "/signup";
+
+//   if (publicPaths && token) {
+//     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+//   }
+//   if (!publicPaths && !token) {
+//     return NextResponse.redirect(new URL("/", req.nextUrl));
+//   }
+// }
+
+export { default } from 'next-auth/middleware'
+
+export const config = {
+  matcher: ["/dashboard", "/companies"],
+};
