3dd049d61f84e66f6cb61a770337a95d5a0274f9
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2024-02-23 14:15:34 +0200

    modify home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/images/logo.png';
+
+const links = [
+  { id: '1', path: '/signin', name: 'Sign in' },
+  { id: '2', path: '/signup', name: 'Sign up' },
+];
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
-      <Link className="text-xl p-2" href="/companies">
-        Go to the Companies
-      </Link>
-    </main>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div className="bg-slate-300 p-3 rounded-xl mx-auto w-fit mb-6">
+              <Image width={100} src={logo} alt="Logo" />
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="pb-8 space-y-2 md:space-y-5">
+                <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                  Welcome to Our Website
+                </h2>
+                <p className="text-lg leading-7 text-gray-500">
+                  We are a company dedicated to providing high-quality services.
+                </p>
+              </div>
+              <div className="w-fit mx-auto gap-6 flex items-center content-between pt-8 md:space-y-5">
+                {links.map((link) => (
+                  <Link
+                    key={link.id}
+                    className="text-base font-medium leading-6 text-indigo-500 hover:text-indigo-600"
+                    href={link.path}
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
